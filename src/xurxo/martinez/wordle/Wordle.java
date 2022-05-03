82be4d5b07425415fe95bbd94d565c87e63a0e43
Xurxo Martínez Ferreira <73365795+XurxoMF@users.noreply.github.com>
2022-05-03 16:49:55 +0200

    Creación do proxecto

---
+++ src/xurxo/martinez/wordle/Wordle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package xurxo.martinez.wordle;
+
+/**
+ *
+ * @author xurxo
+ */
+public class Wordle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+} 
