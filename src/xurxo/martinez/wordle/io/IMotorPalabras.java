a90da17179988365bb2961ec4ccfa94b0dc45776
Xurxo Martínez Ferreira <73365795+XurxoMF@users.noreply.github.com>
2022-05-10 18:11:44 +0200

    Creación interfaz motores

---
+++ src/xurxo/martinez/wordle/io/IMotorPalabras.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package xurxo.martinez.wordle.io;
+
+/**
+ *
+ * @author xurxo
+ */
+public interface IMotorPalabras {
+    
+    public String getPalabraRandom();
+    
+    public boolean existePalabra();
+    
+    public boolean anhadirPalabra();
+    
+    public boolean eliminarPalabra();
+    
+}
