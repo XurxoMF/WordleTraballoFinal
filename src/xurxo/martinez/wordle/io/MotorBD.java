43d13cf250f9883393cb0700a513e0893358f82f
XurxoMF <xurxolaxe@gmail.com>
2022-05-20 10:04:37 +0200

    Creación do documento para o motor de BBDD

---
+++ src/xurxo/martinez/wordle/io/MotorBD.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xurxo.martinez.wordle.io;
+
+/**
+ *
+ * @author alumno
+ */
+public class MotorBD implements IMotorPalabras {
+    
+    
+
+    @Override
+    public String getPalabraRandom() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean existePalabra(String palabra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean anhadirPalabra(String palabra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarPalabra(String palabra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkPalabra(String palabra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
